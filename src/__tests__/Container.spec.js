7304d5bfba0f58fc85643bfe5a8f63b55e7ea776
Lucas Badico <lgsantana1@stefanini.com>
2019-04-30 19:45:15 -0300

    spec layouted

--- src/__tests__/Container.spec.js
+++ src/__tests__/Container.spec.js
@@ -1,9 +1,17 @@
 describe('Container spec - constructor', () => {
     it('Container should be a constructor')
-    it('new Container should return a object with prop services')
+    it('new Container should return a object with property services')
 })
 
 describe('Container spec - service method', () => {
     it('should be able to insert a new service inside a the object')
-    it('new Container should return a object with prop services')
+    it('should services being requested in a lazy way')
+    it('should getter being called just at first time that we use the service')
+})
+
+describe('Container spec - register method', () => {
+    it('should be able to define a service just with a function as parameter, no injection needed')
+    it('should be able to define a service just with a function as parameter, injection needed')
+    it('should be able to define a service with autoCall, no injection needed')
+    it('should be able to define a service with autoCall, injection needed')
 })
